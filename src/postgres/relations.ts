79af498d97f56715555582cdd0ac251bbd47b1f9
spencersm <spencer.g.smith6@icloud.com>
2025-03-24 21:33:06 -0700

    Added newsfeed UI

--- src/postgres/relations.ts
+++ src/postgres/relations.ts
@@ -1,5 +1,44 @@
 import { relations } from "drizzle-orm/relations";
-import { organizations, organizationMembers, users } from "./schema";
+import {
+  users,
+  accounts,
+  organizations,
+  documents,
+  takeaways,
+  organizationMembers,
+} from "./schema";
+
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, {
+    fields: [accounts.userId],
+    references: [users.id],
+  }),
+  organization: one(organizations, {
+    fields: [accounts.organizationId],
+    references: [organizations.id],
+  }),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  accounts: many(accounts),
+  organizationMembers: many(organizationMembers),
+}));
+
+export const organizationsRelations = relations(organizations, ({ many }) => ({
+  accounts: many(accounts),
+  organizationMembers: many(organizationMembers),
+}));
+
+export const takeawaysRelations = relations(takeaways, ({ one }) => ({
+  document: one(documents, {
+    fields: [takeaways.documentId],
+    references: [documents.id],
+  }),
+}));
+
+export const documentsRelations = relations(documents, ({ many }) => ({
+  takeaways: many(takeaways),
+}));
 
 export const organizationMembersRelations = relations(
   organizationMembers,
@@ -14,11 +53,3 @@ export const organizationMembersRelations = relations(
     }),
   }),
 );
-
-export const organizationsRelations = relations(organizations, ({ many }) => ({
-  organizationMembers: many(organizationMembers),
-}));
-
-export const usersRelations = relations(users, ({ many }) => ({
-  organizationMembers: many(organizationMembers),
-}));
