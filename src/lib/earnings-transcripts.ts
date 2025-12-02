52d24835052bba16ff3b7f25bfb5a3a3d3763b4d
spencersm <spencer.g.smith6@icloud.com>
2025-12-02 08:20:03 -0700

    remove old transcripts api

--- src/lib/earnings-transcripts.ts
+++ src/lib/earnings-transcripts.ts
@@ -2,73 +2,20 @@
 import { NonRetriableError } from "inngest";
 import { ensureEnv } from "./env";
 
-const API_BASE_URL = "https://api.api-ninjas.com/v1/earningstranscript";
-const API_KEY = ensureEnv("API_NINJA");
-
 const ALPHAVANTAGE_API_BASE_URL = "https://www.alphavantage.co/query";
 const ALPHAVANTAGE_API_KEY = ensureEnv("ALPHAVANTAGE_API_KEY");
 
-// types/earningsTranscript.ts
-export interface TranscriptSplit {
-  speaker: string;
-  text: string;
-}
-
-export interface EarningsTranscriptResponse {
-  date: string; // e.g. "2024-01-30"
-  transcript: string;
-  transcript_split: TranscriptSplit[];
-}
-
-export interface AlphaVantageTranscriptEntry {
+interface AlphaVantageTranscriptEntry {
   speaker: string;
   title: string;
   content: string;
   sentiment: string;
 }
 
-export interface AlphaVantageEarningsTranscriptResponse {
+interface AlphaVantageEarningsTranscriptResponse {
   symbol: string;
   quarter: string;
   transcript: AlphaVantageTranscriptEntry[];
-}
-
-interface FetchTranscriptParams {
-  ticker: string;
-  year: number;
-  quarter: number;
-}
-
-export async function fetchEarningsTranscript({
-  ticker,
-  year,
-  quarter,
-}: FetchTranscriptParams): Promise<EarningsTranscriptResponse> {
-  const url = `${API_BASE_URL}?ticker=${encodeURIComponent(ticker)}&year=${year}&quarter=${quarter}`;
-  console.log("url", url);
-
-  const response = await fetch(url, {
-    headers: {
-      "X-Api-Key": API_KEY,
-    },
-  });
-
-  if (!response.ok) {
-    console.log("response", response);
-    throw new NonRetriableError(
-      `Failed to fetch ${ticker} transcript: ${response.statusText}`,
-    );
-  }
-
-  const data = await response.json();
-
-  const typedData = data as EarningsTranscriptResponse;
-
-  if (!typedData.date || !typedData.transcript) {
-    throw new NonRetriableError(`Failed to fetch ${ticker} transcript`);
-  }
-
-  return typedData;
 }
 
 interface FetchAlphaVantageTranscriptParams {
