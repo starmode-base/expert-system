3dcc48493b73ea62d7c27bad0e199108195eb310
spencersm <spencer.g.smith6@icloud.com>
2025-12-16 21:05:35 -0700

    clean up

--- src/inngest/functions/index.ts
+++ src/inngest/functions/index.ts
@@ -1,13 +1,11 @@
 import { earningsCallsScraper } from "./earnings-calls-scraper";
 import { generateInsight } from "./generate-insight";
 import { generateTakeaways } from "./generate-takeaways";
-import { helloWorld } from "./hello-world";
 import { processEarningsJobs } from "./process-earnings-jobs";
 import { scienceDailyScraper } from "./science-daily-scraper";
 import { syncEarningsCalendar } from "./sync-earnings-calendar";
 
 export const inngestFunctions = [
-  helloWorld,
   scienceDailyScraper,
   earningsCallsScraper,
   generateTakeaways,
