6924504e1b04bf8876f1f175879c079cff40b4c9
spencersm <spencer.g.smith6@icloud.com>
2026-02-04 10:30:13 -0700

    Delete post to x

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -25,8 +25,6 @@ const rawEnv = {
   X_BEARER_TOKEN: process.env.X_BEARER_TOKEN,
   X_API_KEY: process.env.X_API_KEY,
   X_API_SECRET: process.env.X_API_SECRET,
-  X_ACCESS_TOKEN: process.env.X_ACCESS_TOKEN,
-  X_ACCESS_SECRET: process.env.X_ACCESS_SECRET,
   X_CLIENT_ID: process.env.X_CLIENT_ID,
   X_CLIENT_SECRET: process.env.X_CLIENT_SECRET,
   X_REDIRECT_URI: process.env.X_REDIRECT_URI,
